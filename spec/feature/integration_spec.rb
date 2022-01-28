d09b43842d4e6af26d889d3e4498e03fc1d62fc6
ninjaflautist <ninjaflautist@tamu.edu>
2022-01-28 11:19:55 -0600

    first commit

---
+++ spec/feature/integration_spec.rb
@@ -0,0 +1,12 @@
+# location: spec/feature/integration_spec.rb
+require 'rails_helper'
+
+RSpec.describe 'Creating a book', type: :feature do
+  scenario 'valid inputs' do
+    visit new_book_path
+    fill_in 'Title', with: 'harry potter'
+    click_on 'Create Book'
+    visit books_path
+    expect(page).to have_content('harry potter')
+  end
+end
